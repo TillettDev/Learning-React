e5358ee025644ea30d17b461ae5ff61f98bc59bf
TillettDev <57319893+TillettDev@users.noreply.github.com>
2020-06-06 10:32:04 +0100

    Updates

---
+++ precache-manifest.705f4549c5de627135740454dd8e9a3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fca444c5b43532081559e150c0b9dbc",
+    "url": "/Learning-React/index.html"
+  },
+  {
+    "revision": "5fb1d3b33a40b66c98e9",
+    "url": "/Learning-React/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fa1edb29574ed4f447da",
+    "url": "/Learning-React/static/js/2.f3d1383b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Learning-React/static/js/2.f3d1383b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fb1d3b33a40b66c98e9",
+    "url": "/Learning-React/static/js/main.392602eb.chunk.js"
+  },
+  {
+    "revision": "ba64d713d00af37dfed6",
+    "url": "/Learning-React/static/js/runtime-main.1ba2c171.js"
+  }
+]);
