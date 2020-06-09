0891eeb450a3a42ebf473d0853a829a5870d3e1b
TillettDev <57319893+TillettDev@users.noreply.github.com>
2020-06-09 23:05:40 +0100

    added cards, passed products down to product items

--- src/App.js
+++ src/App.js
@@ -12,13 +12,22 @@ const items = [
   {id: 3, name: 'Cheese', price: 3.25, qty: 3},
 ]
 
+const products = [
+  {id: 1, name: 'Apple', img: './images/Apple.png', desc: 'Apple', price: 0.25, inStock: 20},
+  {id: 2, name: 'Grapes', img: './images/Grapes.png', desc: 'Grapes', price: 2.25, inStock: 20},
+  {id: 3, name: 'Orange', img: './images/Orange.png', desc: 'Orange', price: 2.25, inStock: 25},
+  {id: 4, name: 'Strawberries', img: './images/Strawberries.png', desc: 'Strawberries', price: 2.50, inStock: 15},
+  {id: 5, name: 'Watermelon', img: './images/Watermelon.png', desc: 'Watermelon', price: 3, inStock: 10},
+  {id: 6, name: 'Avacado', img: './images/Avacado.png', desc: 'Avacado', price: 3, inStock: 10},
+];
+
 function App() {
   return (
     <Router>
       <Navigation />
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/products" component={Products}/>
+        <Route path="/products" render={(props) => <Products {...props} products={products}/> } />
         <Route path="/cart" render={(props) => <Cart {...props} initialItems={items} />}/>
       </Switch>
     </Router>
